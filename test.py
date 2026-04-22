bd703c740b698389796f89980d96b97f118b0b81
Aziz Ben Hammouda <azizbhammouda@gmail.com>
2026-04-22 22:11:29 +0100

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from langchain_groq import ChatGroq
-
-llm = ChatGroq(
-    temperature=0,
-    model_name="llama-3.3-70b-versatile"
-)
-
-response = llm.invoke("Explain what a firewall does in one sentence")
-print(response.content)
